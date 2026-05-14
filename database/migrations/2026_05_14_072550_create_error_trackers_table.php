f815168a623789002664e95fc9a65764a70f7905
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-05-14 13:15:30 +0545

    Update the ErrorTracker migration
    Add the definitions for the columns of the 'error_trackers' table

--- database/migrations/2026_05_14_072550_create_error_trackers_table.php
+++ database/migrations/2026_05_14_072550_create_error_trackers_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -13,7 +15,20 @@ return new class extends Migration
     {
         Schema::create('error_trackers', function (Blueprint $table) {
             $table->id();
+            $table->string('region');
+            $table->string('branch');
+            $table->integer('application_id')->nullable();
+            $table->string('issuse');
+            $table->string('impact')->nullable();
+            $table->text('root_cause')->nullable();
+            $table->time('estimated_down')->nullable();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->text('issue_triggered_by')->nullable();
+            $table->string('error_message')->nullable();
+            $table->string('severity');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
