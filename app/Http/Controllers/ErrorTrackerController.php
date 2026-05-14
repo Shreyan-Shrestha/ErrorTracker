91db19b3900fc9be190d047f66213bf816bcf942
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-05-14 13:13:09 +0545

    Create model, -mrc for ErrorTracker
    Create model, migration and resource controller for ErrorTracker

---
+++ app/Http/Controllers/ErrorTrackerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ErrorTracker;
+use Illuminate\Http\Request;
+
+class ErrorTrackerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ErrorTracker $errorTracker)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ErrorTracker $errorTracker)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ErrorTracker $errorTracker)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ErrorTracker $errorTracker)
+    {
+        //
+    }
+}
