e5658478b5dae2aa8d1f30c3cbc7cf8cde732b42
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-05-19 16:38:54 +0545

    Update config/auth.php
    Add login guard to auth.pp

--- config/auth.php
+++ config/auth.php
@@ -42,6 +42,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'login' => [
+            'driver' => 'session',
+            'provider' => 'users'
+        ]
     ],
 
     /*
