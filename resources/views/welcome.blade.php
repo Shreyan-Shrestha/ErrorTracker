25c94ac50c5734e3a0a26a8d1cb1c9f2ef7ed630
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-05-19 13:44:36 +0545

    Add ApplicationRequest edit welcome.blade.php and route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,31 @@
- <style>
-        input[type="text"] {
-            font-size: 16px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-
-        input[type="text"]:focus {
-            border-color: #66afe9;
-            outline: none;
-        }
-    </style>
 <x-layout title="Homepage | ErrorTracker" class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-    <div class="flex container transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-        <form class="mt-3 p-3 w-16 md:w-32 lg:w-64 mx-auto flex flex-col col" action="{{ route('errors.store') }}" method="POST">
+    <div class="transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+        <form class="mt-3 p-3 md:w-64 lg:w-98 mx-auto grid gap-3 w-full" action="{{ route('errors.store') }}" method="POST">
             @csrf
-            <label for="region">Region:</label>
-            <input class="outline-amber-300" type="text" name="region" placeholder="Enter Region"></input>
-            <label for="branch">Branch:</label>
-            <input class="outline-amber-300" name="branch" type="text"/>
-            <label for="issue" >Issue</label>
-            <input name="issue" class="outline-amber-300" type="text"/>
-            <label class="w-full mt-3" for="start_time">Select Start time:</label>
-            <input type="datetime" id="nepali-datepicker-with-mini-english-dates" name="start_time" class="mt-3" placeholder="Select Date:" />
-            <label for="severity">Severity</label>
-            <input type="text" class="outline-amber-300" name="severity"/>
-            <button class="bg-blue-500 text-white p-3 mt-3" type="submit">Store</button>
+            <fieldset class="fieldset rounded-box border p-4 flex">
+                <legend class="legend">Location</legend>
+            <label class="input w-full">
+                <input type="text" class="grow" name="region" placeholder="Enter Region" autocomplete="off">
+            </label>
+
+            <label class="input w-full">
+                <input name="branch" class="grow" type="text" placeholder=" Branch" autocomplete="off">
+            </label>
+            </fieldset>
+
+            <label class="input w-full mt-5">
+                <input name="issue" class="grow" type="text" placeholder="Issue" autocomplete="off">
+            </label>
+
+            <label class="input w-full">
+                <input type="text" class="grow" name="severity" placeholder="Severity" autocomplete="off">
+            </label>
+
+            <label class="input w-full" for="nepali-datepicker-with-mini-english-dates">
+                <input type="datetime" class="grow" id="nepali-datepicker-with-mini-english-dates" name="start_time" class="grow" placeholder="Select Start Time" autocomplete="off">
+            </label>
+
+            <button class="btn btn-primary mt-5" type="submit">Store</button>
         </form>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\ErrorTrackerController;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 use App\Models\ErrorTracker;
-use Carbon\Carbon;
-use RohanAdhikari\NepaliDate\NepaliDate;
+use App\Http\Controllers\ApplicationController;
 
-Route::get('/', function () {
-    $now = ErrorTracker::latest()->first();
-    return view('welcome', compact('now'));
+Route::get('/', [ErrorTrackerController::class, 'index']);
+
+Route::prefix('errors')->name('errors.')->group(function() {
+Route::post('/store', [ErrorTrackerController::class, 'store'])->name('store');
 });
 
-Route::post('/store', [ErrorTrackerController::class, 'store'])->name('errors.store');
+Route::resource('applications', ApplicationController::class);
