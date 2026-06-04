de49492e65e7b70dcc03a5ae47cf073d7b47c274
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-06-04 15:53:56 +0545

    Create and register ResponseMacroServiceProvider

---
+++ app/Providers/ResponseMacroServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\ServiceProvider;
+
+class ResponseMacroServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Response::macro('success',function (
+            mixed $data, 
+            ?string $message = 'Request completed successfully', 
+            $status = 200
+            ): JsonResponse 
+            {
+                return Response::json([
+                    'success' => 'true',
+                    'message' =>  $message,
+                    'data' => $data,
+                ], $status, [
+                    'Content-Type' => 'application/vnd.api+json'
+                ]);
+            }
+        );
+
+        Response::macro('error', function (string $message = 'Error!', int $status = 400, mixed $errors = null): JsonResponse {
+            return Response::json([
+                'success' => 'false',
+                'message' => $message,
+                'errors' => $errors,
+            ], $status, [
+                'Content-Type' => 'application/vnd.api+json',
+            ]);
+        });
+    }
+}
