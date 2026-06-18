2b61c1a9b4f694794a19d3232ed2aa04c4ff924e
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-06-18 13:32:56 +0545

    Create color and status functions in ProjectStatus enum
    Create functions color() and status() to pass classes for setting color for status badge and status in Project Blade view

--- app/Enums/ProjectStatus.php
+++ app/Enums/ProjectStatus.php
@@ -13,7 +13,24 @@ enum ProjectStatus : string
         return match($this){
             ProjectStatus::Completed  => 'completed',
             ProjectStatus::Ongoing    => 'ongoing',
-            ProjectStatus::Abandoned  => 'abandooned',
+            ProjectStatus::Abandoned  => 'abandoned',
+        };
+    }
+
+    public function color(): string
+    {
+        return match($this){
+            ProjectStatus::Completed => 'py-2 rounded-box bg-green-200 text-success-content',
+            ProjectStatus::Ongoing => 'py-2 rounded-box bg-orange-200 text-warning-content',
+            ProjectStatus::Abandoned => 'py-2 rounded-box bg-red-200 text-error-content',
+        };
+    }
+
+    public function  status() : string {
+        return match($this){
+            ProjectStatus::Completed => 'bg-green-800',
+            ProjectStatus::Ongoing => 'bg-orange-800',
+            ProjectStatus::Abandoned => 'bg-red-800',
         };
     }
 }
