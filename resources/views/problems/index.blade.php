c1a412b134400236e124866f6f25ff741e388d04
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-06-22 12:49:35 +0545

    Add Routes for Problem, Add functionality for Problems and Category and add the modal.create to implement it. Add Edit and Delete functionality through modal.create, OpenEditModal method, and modal.delete

--- resources/views/problems/index.blade.php
+++ resources/views/problems/index.blade.php
@@ -1,4 +1,4 @@
-@extends('partials.layout', ['title', 'WorldLink ErrorTracker | Problems'])
+@extends('partials.layout', ['title' => 'WorldLink ErrorTracker | Problems'])
 
 @section('content')
 <div class="w-full p-6">
@@ -7,23 +7,23 @@
             <p class="text-2xl lg:text-4xl font-bold">Problems</p>
         </div>
         <div class="grid justify-items-start gap-4">
-            <x-buttons.primary>Add Problem</x-buttons.primary>
-            <x-buttons.primary>Add Category</x-buttons.primary>
+            <x-buttons.primary onClick="OpenCreateModal(this.dataset.action, 'modal_problem')" dataAction="{{ route('problems.create') }}">Add Problem</x-buttons.primary>
+            <x-buttons.primary onClick="OpenCreateModal(this.dataset.action, 'modal_category')" dataAction="{{ route('category.create') }}">Add Category</x-buttons.primary>
         </div>
 
     </div>
 
-    <div class="grid grid-cols-2 sm:grid-cols-4 gap-6 mt-6">
-        <x-stat-card title="Active Problems" value="24" change="+4 this week" desc="open issues" iconColor="text-primary" iconBg="bg-blue-100">
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-6 mt-6">
+        <x-stat-card class="border-s-4 border-s-primary" title="Active Problems" value="24" change="+4 this week" desc="open issues" iconColor="text-primary" iconBg="bg-blue-100">
             <x-slot:icon>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person-vcard" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-vcard" viewBox="0 0 16 16">
                     <path d="M5 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4m4-2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5M9 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4A.5.5 0 0 1 9 8m1 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5" />
                     <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM1 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H8.96q.04-.245.04-.5C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 1 1 12z" />
                 </svg>
             </x-slot:icon>
         </x-stat-card>
 
-        <x-stat-card title="Resolution Time" value="12.2h" change="average time" desc="All time" iconColor="text-success-content" iconBg="bg-green-100">
+        <x-stat-card class="border-s-4 border-s-success" title="Resolution Time" value="12.2h" change="Avg. time" desc="All time" iconColor="text-success-content" iconBg="bg-green-100">
             <x-slot:icon>
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="18" height="18" viewBox="0 0 24 24" style="color: rgb(74, 85, 101);">
                     <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 18A10 10 0 1 0 2.9 7.9M2 4v4h4m6-1v5l3 3"></path>
@@ -31,7 +31,7 @@
             </x-slot:icon>
         </x-stat-card>
 
-        <x-stat-card title="Team Efficiency" value="94%" change="" desc="5 teams" iconColor="text-warning-content" iconBg="bg-orange-100">
+        <x-stat-card class="border-s-4 border-s-warning" title="Team Efficiency" value="94%" change="" desc="5 teams" iconColor="text-warning-content" iconBg="bg-orange-100">
             <x-slot:icon>
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-radioactive" viewBox="0 0 16 16">
                     <path d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8" />
@@ -40,8 +40,8 @@
             </x-slot:icon>
         </x-stat-card>
 
-        <div class="stat border border-s-4 border-s-red-700 text-red-700 border-gray-400 bg-red-50 shadow rounded-md p-6">
-            <div class="stat-figure [grid-row:1] p-2 rounded bg-red-100 text-red-800">
+        <div class="stat border border-s-4 border-s-red-700 text-red-700 border-gray-400 bg-red-50 shadow rounded-md p-2 xl:p-6">
+            <div class="stat-figure [grid-row:1] p-2 rounded bg-red-100 text-red-800 self-start md:hidden lg:flex">
                 <span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
                         <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z" />
@@ -59,5 +59,106 @@
         </div>
     </div>
 
+    <div class="grid gird-cols-1 md:grid-cols-3 gap-6 mt-6">
+        <div class="col-1 rounded-box border border-gray-400 mt-6 p-2 md:p-4">
+            <p class="text-3xl">Problem Categories</p>
+            <div class="px-6">
+                <ul class="mt-6 grid justify-center">
+                    @if($categories->isEmpty())
+                    <li>Not Added Yet</li>
+                    @else
+                    @foreach($categories as $category)
+                    <li class="font-semibold text-xl">
+                        <div aria-label="status" class="status {{ $category->severity->status() }}"></div> {{$category->name}}
+                    </li>
+                    @endforeach
+                    @endif
+                </ul>
+            </div>
+        </div>
+
+        @php
+        $headers = ['Name', 'Severity', 'Status', 'Actions'];
+        $count = count($headers);
+        @endphp
+        <div class="col-1 md:col-span-2">
+            <x-table :headers="$headers" title="Problems">
+                @if($problems->isEmpty())
+                <tr class="bg-white hover:bg-base-300 p-4">
+                    <td colspan="{{ $count }}" class="px-4 py-3 text-center text-gray-500">
+                        No Problems added yet.
+                    </td>
+                </tr>
+                @else
+                @foreach( $problems as $problem )
+                <tr class="bg-white hover:bg-base-300 p-4">
+                    <td> {{ $problem->name }}</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="flex gap-4 items-center-safe justify-center-safe">
+                            <button class="link" 
+                            data-modal="modal_edit_problem" 
+                            data-action="{{route('problems.edit', $problem)}}" 
+                            onclick="OpenEditModal(this)" 
+                            data-name="{{ $problem->name }}">
+                                Edit
+                            </button>
+
+                            <button class="link hover:text-warning-content delete-btn"
+                                data-action="{{ route('problems.delete', $problem->id) }}">
+                                Delete
+                            </button>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+                @endif
+            </x-table>
+        </div>
+    </div>
+
+    <x-modal.create id="modal_problem" creating="Add Problem">
+        <div class="p-3 px-6 grid gap-4">
+            <label class="fieldset">
+                <span class="label">PROBLEM NAME</span>
+                <input type="text" class="input input-sm sm:input-md validator" required name="name" placeholder="Enter Problem Name">
+                <p class="validator-hint hidden">Required</p>
+            </label>
+        </div>
+    </x-modal.create>
+
+    <x-modal.create id="modal_category" creating="Add Problem Category">
+        <div class="p-3 px-6 grid gap-4">
+            <label class="fieldset">
+                <span class="label">Category Name</span>
+                <input type="text" class="input input-sm sm:input-md validator" name="name"
+                    validator required placeholder="Enter Problem Name">
+                <div class="validator-hint hidden">Required</div>
+            </label>
+            <label class="fieldset">
+                <span class="label">SEVERITY</span>
+                <select class="select" required name="severity">
+                    <option value="" disabled selected>Category Severity</option>
+                    @foreach(App\Enums\ErrorSeverity::cases() as $severity)
+                    <option value="{{$severity->value}}"> {{ $severity->label() }} </option>
+                    @endforeach
+                </select>
+                <div class="validator-hint hidden">Required</div>
+            </label>
+        </div>
+    </x-modal.create>
+
+    <x-modal.create id="modal_edit_problem" creating="Edit Problem" methodPatch="true">
+        <div class="p-3 px-6 grid gap-4">
+            <input type="hidden" name="problem_id" id="edit_problem_id">
+            <label class="fieldset">
+                <span class="label">PROBLEM NAME</span>
+                <input type="text" class="input input-sm sm:input-md validator" required name="name" id="edit_problem_name" placeholder="Enter Problem Name">
+                <p class="validator-hint hidden">Required</p>
+            </label>
+        </div>
+    </x-modal.create>
+    <x-modal.delete toDelete="Problem"></x-modal.delete>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\Web\ApplicationController;
 use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\auth\UserAuthenticationController;
 use App\Http\Controllers\Auth\WebLogin;
+use App\Http\Controllers\Web\CategoryController;
 use App\Http\Controllers\Web\ErrorTrackerController;
+use App\Http\Controllers\Web\ProblemController;
 use App\Http\Controllers\Web\ProjectController;
+use App\Models\Category;
 
 Route::get('/', [ErrorTrackerController::class, 'index']);
 
@@ -31,6 +34,18 @@ Route::middleware('login')->group(function () {
         Route::post('/', [ProjectController::class, 'store'])->name('create');
         Route::patch('/{project}', [ProjectController::class, 'update'])->name('edit');
         Route::delete('/delete/{project}', [ProjectController::class, 'destroy'])->name('delete');
+    });
+
+    Route::prefix('problems')->name('problems.')->group(function() {
+        Route::get('/', [ProblemController::class, 'index'])->name('index');
+        Route::post('/', [ProblemController::class,'store'])->name('create');
+        Route::patch('/{problem}', [ProblemController::class, 'update'])->name('edit');
+        Route::delete('/delete/{problem}',[ProblemController::class,'destroy'])->name('delete');
+    });
+
+    Route::prefix('category')->name('category.')->group( function(){
+        Route::post('/', [CategoryController::class, 'store'])->name('create');
+        Route::delete('/delete/{category}', [CategoryController::class, 'destroy'])->name('delete');
     });
 
     Route::get('/users', function () {
