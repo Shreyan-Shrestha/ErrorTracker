668c886c724987241d4a897933011fb73b8b50dc
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-06-29 16:14:37 +0545

    Add In Review case to the enum

--- app/Enums/ProjectStatus.php
+++ app/Enums/ProjectStatus.php
@@ -7,6 +7,7 @@ enum ProjectStatus : string
     case Completed = 'completed';
     case Ongoing = 'ongoing';
     case Abandoned = 'abandoned';
+    case In_Review = 'in review';
 
     public function label() : string
     {
@@ -14,6 +15,7 @@ enum ProjectStatus : string
             ProjectStatus::Completed  => 'completed',
             ProjectStatus::Ongoing    => 'ongoing',
             ProjectStatus::Abandoned  => 'abandoned',
+            ProjectStatus::In_Review => 'in review'
         };
     }
 
@@ -23,14 +25,16 @@ enum ProjectStatus : string
             ProjectStatus::Completed => 'py-2 rounded-box bg-green-200 text-success-content',
             ProjectStatus::Ongoing => 'py-2 rounded-box bg-orange-200 text-warning-content',
             ProjectStatus::Abandoned => 'py-2 rounded-box bg-red-200 text-error-content',
+            ProjectStatus::In_Review => 'py-2 rounded-box bg-blue-200 text-info-content',
         };
     }
 
     public function  status() : string {
         return match($this){
-            ProjectStatus::Completed => 'bg-green-800',
-            ProjectStatus::Ongoing => 'bg-orange-800',
-            ProjectStatus::Abandoned => 'bg-red-800',
+            ProjectStatus::Completed => 'bg-green-700',
+            ProjectStatus::Ongoing => 'bg-orange-700',
+            ProjectStatus::Abandoned => 'bg-red-700',
+            ProjectStatus::In_Review => 'bg-blue-700',
         };
     }
 }
