6104095f8588319c756042ee498f447903f345a0
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-07-29 13:44:44 +0545

    Rename getAllCount() method to getLogCount() and update the method to count ErrorReport count

--- app/Repositories/Api/ProblemRepository.php
+++ app/Repositories/Api/ProblemRepository.php
@@ -16,9 +16,9 @@ class ProblemRepository implements ProblemRepositoryInterface
         ->paginate(10);
     }
 
-    public function getAllCount(): int
+    public function getLogCount(): int
     {
-        return Problem::count();
+        return ErrorReport::count();
     }
 
     public function getById(int $id) : Problem
